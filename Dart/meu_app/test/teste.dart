13f9a73a827e6d70714d82a4297dcc02c00e9fcc
Lorenzo Lopes <lorenzo.lopes0808@gmail.com>
2025-08-13 22:49:31 -0300

    Update teste.dart

--- Dart/meu_app/test/teste.dart
+++ Dart/meu_app/test/teste.dart
@@ -1,8 +1,18 @@
-import 'dart:io';
-
 void main() {
-  print('Digite seu nome:');
-  String? nome = stdin.readLineSync();
+  //for
+  final List<int> numeros = [10, 20, 30, 40];
 
-  print("Olá, $nome!");
+  for (int indice = 0; indice < numeros.length; indice++) {
+    final int valorAtual = numeros[indice];
+    print('[$indice] => $valorAtual');
+  }
+
+  //for "infinito com condição e saida controlada"
+
+  int contador = 0;
+  for (;;) {
+    if (contador >= 3) break;
+    print('Contagem: $contador');
+    contador++;
+  }
 }
