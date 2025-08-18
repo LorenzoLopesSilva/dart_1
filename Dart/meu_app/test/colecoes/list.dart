5fa4fdbddfa7411a9c3b295296afa9b56d050071
ETS-EngineeringTechnicalSchool BOT-ResearchDevelopment (CaP/ETS) <ct67ca@bosch.com>
2025-08-18 14:48:34 -0300

    teste

---
+++ Dart/meu_app/test/colecoes/list.dart
@@ -0,0 +1,20 @@
+void main(){
+  //Lista de inteiros
+  List<int> numeros = [1, 2, 3, 4, 5];
+  
+  //Lista dinamica (pode ter tipos diferentes)
+  List dinamica = [1, 'texto', true];
+  
+  print(numeros);
+  print(dinamica);
+
+    List<String> nomes = ['Ana', 'Beatriz', 'Carlos'];
+  
+  print(nomes[0]);
+  nomes.add('Daniel'); //adicionar
+  nomes.remove('Beatriz'); //Remover pelo valor
+  nomes.removeAt(0); //Remover pelo indice
+  print(nomes.contains('Carlos'));
+  print(nomes.length);
+  
+}
